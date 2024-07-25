d20d24c816e490db816dae54c7645d9fa1459aae
Aneegato <aneeshmallick2002@gmail.com>
2024-07-25 12:55:16 +0900

    add

--- Frontend/src/axiosConfig.js
+++ Frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = import.meta.env.MODE === 'production'
-  ? 'http://timenus-environment.eba-8xnttsva.ap-southeast-1.elasticbeanstalk.com/' // Replace with your production backend URL
+  ? 'https://timenus-backend.vercel.app/' // Replace with your production backend URL
   : '/api'; // This will be proxied to your local backend server
 
 const instance = axios.create({

eb1f5cf4af30cdb2e12bf3bc006774e000ae9c58
Aneegato <aneeshmallick2002@gmail.com>
2024-07-25 23:47:18 +0900

    update

--- Backend/index.js
+++ Backend/index.js
@@ -11,26 +11,22 @@ app.use(express.urlencoded({ extended: false }));
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://realtimenus.vercel.app',
-  'https://d3c0-218-144-19-223.ngrok-free.app'
+  'https://realtimenus.vercel.app'
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    console.log('Request origin:', origin); // Log each request origin
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      console.log('Blocked by CORS:', origin);
       callback(new Error('Not allowed by CORS'));
     }
   },
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
-
 
 app.use(cors(corsOptions));
 
@@ -52,20 +48,13 @@ const authRoutes = require('./routes/authRoutes');
 app.use('/calendars', calendarRoutes);
 app.use('/events', eventRoutes);
 app.use('/auth', authRoutes);
-app.use('/', userRoutes);
+app.use('/users', userRoutes);
 
-// Add a route handler for '/'
 app.get('/', (req, res) => {
   res.send('Welcome to the backend!');
 });
 
-// Health Check Route
-app.get('/health', (req, res) => {
-  res.status(200).send('Healthy');
-});
-
-// Server listening on HTTP
-const port = process.env.PORT || 5001;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });

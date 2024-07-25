2ae2157b78feae38609e93b27925789296586c04
Aneegato <aneeshmallick2002@gmail.com>
2024-07-25 16:48:10 +0900

    update

--- Frontend/src/axiosConfig.js
+++ Frontend/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = import.meta.env.MODE === 'production'
   ? import.meta.env.VITE_APP_API_URL // Ensure this environment variable is set in your production environment
-  : 'http://localhost:5001'; // Replace with your local backend URL
+  : 'https://d3c0-218-144-19-223.ngrok-free.app'; // Replace with your local backend URL
 
 const instance = axios.create({
   baseURL,

535b3a6e65b4a06503ef40796945fb8e4d5b1f81
Aneegato <aneeshmallick2002@gmail.com>
2024-07-26 01:39:04 +0900

    update

--- Backend/index.js
+++ Backend/index.js
@@ -26,7 +26,7 @@ const corsOptions = {
   credentials: true, // Allow cookies and other credentials
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 app.use(cors(corsOptions));
@@ -34,15 +34,6 @@ app.use(cors(corsOptions));
 // Handle preflight requests for all routes
 app.options('*', cors(corsOptions));
 
-app.use((req, res, next) => {
-    res.cookie('yourCookieName', 'yourCookieValue', {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'None', // Ensure this is correctly set
-    });
-    next();
-  });
-  
 // Connect to MongoDB
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('DB Connected!'))
